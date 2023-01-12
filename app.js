74a6e41f37549aeeff62fe9a83cdb056cd1b1328
ISHIMWE-Aime <fabricei645@gmail.com>
2023-01-12 15:11:02 +0200

    intergation changes

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const specs = swaggerJsDoc(options) //initialise swaggerJsDoc
 const app = express()
 
 let corsOptions = 		{
-	origin:'https://ishimwe-aime.github.io',
+	origin: true,
 	credentials:  true
 }
 
